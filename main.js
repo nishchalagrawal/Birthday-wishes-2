38a045e0235f7de1e617515713bb283acc970a0f
nishchalagrawal <80205568+nishchalagrawal@users.noreply.github.com>
2021-08-12 01:26:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var canvas = new fabric.Canvas('my_canvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL("download(2).jpg", function(img)
+	{
+block_image_object = img;
+block_image_object.scaletowidth(700);
+block_image_object.scale.height(510);
+block_image_object.set({
+
+top:0,
+left:0
+});
+
+canvas.add(block_image_object);
+
+
+});
+	
+}
+
+function playsound(){
+	x.play();
+}
